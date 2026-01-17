e10973c719889f216057ea637e33542c16f6c32b
instinctaman <166024431+instinctaman@users.noreply.github.com>
2026-01-17 21:05:07 +0530

    new_profile2

--- src/TypedText.js
+++ src/TypedText.js
@@ -10,7 +10,7 @@ function TypedText() {
       strings: [
         "Full Stack Developer.",
         "Frontend Designer.",
-        "Backend Designer.",
+        "Backend Developer.",
         "UI/UX Enthusiast.",
         "Consultant.",
       ],
